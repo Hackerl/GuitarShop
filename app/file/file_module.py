a91916cf737602cf3e068038ca38b4e6d42f6be0
Hackerl <liupan052997@gmail.com>
2018-05-02 19:08:27 -0400

    fix#picture url

--- app/file/file_module.py
+++ app/file/file_module.py
@@ -2,6 +2,7 @@ from app import photos
 from app.error import ERROR
 from flask_uploads import UploadNotAllowed
 from datetime import datetime
+from urllib import parse
 import os
 
 class file_module:
@@ -15,7 +16,7 @@ class file_module:
             file.filename = 'picture_name'
             filename = photos.save(file, 'picture', name='%s.%s' % (datetime.now().isoformat(), extension))
             file_url = photos.url(filename)
-            return ERROR.success({'filename':realname, 'picture_url': file_url})
+            return ERROR.success({'filename':realname, 'picture_url': parse.urlparse(file_url).path})
 
         except UploadNotAllowed:
             return ERROR.UPLOAD_FAILD
