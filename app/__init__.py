4a69ddb2aebc675e6a1acb41ed5048598ebfa61d
Hackerl <liupan052997@gmail.com>
2018-05-03 20:38:33 -0400

    change#url prefix

--- app/__init__.py
+++ app/__init__.py
@@ -16,28 +16,28 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
 
     from app.user.user_blueprint import user_handler as user_blueprint
-    app.register_blueprint(user_blueprint)
+    app.register_blueprint(user_blueprint,  url_prefix='/user')
 
     from app.defend.defend_blueprint import defend_handler as defend_blueprint
-    app.register_blueprint(defend_blueprint)
+    app.register_blueprint(defend_blueprint,  url_prefix='/check')
 
     from app.contact.contact_blueprint import contact_handler as contact_blueprint
-    app.register_blueprint(contact_blueprint)
+    app.register_blueprint(contact_blueprint,  url_prefix='/contact')
 
     from app.admin.admin_blueprint import admin_handler as admin_blueprint
-    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(admin_blueprint,  url_prefix='/admin')
 
     from app.suggest.suggest_blueprint import suggest_handler as suggest_blueprint
-    app.register_blueprint(suggest_blueprint)
+    app.register_blueprint(suggest_blueprint,  url_prefix='/suggest')
 
     from app.wiki.wiki_blueprint import wiki_handler as wiki_blueprint
-    app.register_blueprint(wiki_blueprint)
+    app.register_blueprint(wiki_blueprint,  url_prefix='/wiki')
 
     from app.file.file_blueprint import file_handler as file_blueprint
-    app.register_blueprint(file_blueprint)
+    app.register_blueprint(file_blueprint,  url_prefix='/file')
 
     from app.teaching.teaching_blueprint import teaching_handler as teaching_blueprint
-    app.register_blueprint(teaching_blueprint)
+    app.register_blueprint(teaching_blueprint,  url_prefix='/teaching')
 
     db.init_app(app)
     socketio.init_app(app)
--- app/admin/admin_blueprint.py
+++ app/admin/admin_blueprint.py
@@ -4,67 +4,67 @@ from app.auth import auth_check, level_define
 
 admin_handler = Blueprint('admin_handler', __name__)
 
-@admin_handler.route('/admin_allissues' , methods=['GET'])
+@admin_handler.route('/issues' , methods=['GET'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_all_issues_route(userid = -1):
     msg = admin_module.get_all_issues()
     return jsonify(msg)
 
-@admin_handler.route('/admin_updateissue' , methods=['POST'])
+@admin_handler.route('/updateissue' , methods=['POST'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_update_issue_route(userid = -1):
     request_json = request.json
     msg = admin_module.update_issue(request_json)
     return jsonify(msg)
 
-@admin_handler.route('/admin_delissue' , methods=['POST'])
+@admin_handler.route('/delissue' , methods=['POST'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_delete_issue_route(userid = -1):
     request_json = request.json
     msg = admin_module.delete_issue(request_json)
     return jsonify(msg)
 
-@admin_handler.route('/admin_queryissue' , methods=['POST'])
+@admin_handler.route('/queryissue' , methods=['POST'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_get_issue_route(userid = -1):
     request_json = request.json
     msg = admin_module.get_issue_content(request_json)
     return jsonify(msg)
 
-@admin_handler.route('/admin_all_suggestions' , methods=['GET'])
+@admin_handler.route('/suggestions' , methods=['GET'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_get_all_suggestion_route(userid = -1):
     msg = admin_module.get_all_suggestions()
     return jsonify(msg)
 
-@admin_handler.route('/admin_query_suggestion' , methods=['POST'])
+@admin_handler.route('/querysuggestion' , methods=['POST'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_get_suggestion_route(userid = -1):
     request_json = request.json
     msg = admin_module.get_suggestion_content(request_json)
     return jsonify(msg)
 
-@admin_handler.route('/admin_getstaffs' , methods=['GET'])
+@admin_handler.route('/getstaffs' , methods=['GET'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_get_staffs_route(userid = -1):
     msg = admin_module.get_staffs()
     return jsonify(msg)
 
-@admin_handler.route('/admin_addstaff' , methods=['POST'])
+@admin_handler.route('/addstaff' , methods=['POST'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_add_staff_route(userid = -1):
     request_json = request.json
     msg = admin_module.add_staff(request_json)
     return jsonify(msg)
 
-@admin_handler.route('/admin_delstaff' , methods=['POST'])
+@admin_handler.route('/delstaff' , methods=['POST'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_del_staff_route(userid = -1):
     request_json = request.json
     msg = admin_module.del_staff(request_json)
     return jsonify(msg)
 
-@admin_handler.route('/admin_queryuser' , methods=['POST'])
+@admin_handler.route('/queryuser' , methods=['POST'])
 @auth_check(level = level_define['ADMIN_LEVEL'])
 def admin_query_userinfo_route(userid = -1):
     request_json = request.json
